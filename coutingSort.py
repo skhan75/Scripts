3bd4af81e22181d261c6219dbf8d790c3d3b2a70
skhan75 <samiahmadkhan@Samis-MacBook-Pro.local>
2016-09-24 12:56:08 -0500

    changes

--- coutingSort.py
+++ coutingSort.py
@@ -1,119 +1,58 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 38,
-   "metadata": {
-    "collapsed": false
-   },
-   "outputs": [],
-   "source": [
-    "def countingSort(A):\n",
-    "    \n",
-    "    minimum = A[0]\n",
-    "    maximum = A[0]\n",
-    "    \n",
-    "    #Find mimimum and maximum for range of index\n",
-    "    for i in A:\n",
-    "        if(i < minimum):\n",
-    "            minimum = i\n",
-    "        if(i > maximum):\n",
-    "            maximum = i\n",
-    "    \n",
-    "    print ('Minimum Element: ', minimum)\n",
-    "    print ('Maximum Element: ', maximum)  \n",
-    "    \n",
-    "    index = []\n",
-    "    count = [0] * (maximum + 1)\n",
-    "    \n",
-    "    \n",
-    "    for i in range(minimum, maximum):\n",
-    "        count[i] = 0 \n",
-    "        \n",
-    "    #Count occurences of each no in array \n",
-    "    for i in A:\n",
-    "        count[A[i]] += 1\n",
-    "        \n",
-    "    \n",
-    "    \n",
-    "    \n",
-    "    \n",
-    "    count_array = [minimum, maximum]\n",
-    "    j = minimum\n",
-    "    \n",
-    "    for i in range(minimum, maximum):\n",
-    "        if(i == ):\n",
-    "            count_array[i] = count[A[j]] \n",
-    "    \n",
-    "        \n",
-    "    \n",
-    "\n",
-    "        \n",
-    "    "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 39,
-   "metadata": {
-    "collapsed": false
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Minimum Element:  1\n",
-      "Maximum Element:  45\n"
-     ]
-    },
-    {
-     "ename": "IndexError",
-     "evalue": "list index out of range",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[0;31mIndexError\u001b[0m                                Traceback (most recent call last)",
-      "\u001b[0;32m<ipython-input-39-8df4313a61f8>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m()\u001b[0m\n\u001b[0;32m----> 1\u001b[0;31m \u001b[0mcountingSort\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m[\u001b[0m\u001b[0;36m2\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m45\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m1\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m5\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m2\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m4\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m7\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;36m8\u001b[0m\u001b[0;34m]\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m",
-      "\u001b[0;32m<ipython-input-38-4a24be6f8d2b>\u001b[0m in \u001b[0;36mcountingSort\u001b[0;34m(A)\u001b[0m\n\u001b[1;32m     21\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     22\u001b[0m     \u001b[0;32mfor\u001b[0m \u001b[0mi\u001b[0m \u001b[0;32min\u001b[0m \u001b[0mA\u001b[0m\u001b[0;34m:\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m---> 23\u001b[0;31m         \u001b[0mcount\u001b[0m\u001b[0;34m[\u001b[0m\u001b[0mA\u001b[0m\u001b[0;34m[\u001b[0m\u001b[0mi\u001b[0m\u001b[0;34m]\u001b[0m\u001b[0;34m]\u001b[0m \u001b[0;34m+=\u001b[0m \u001b[0;36m1\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m\u001b[1;32m     24\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     25\u001b[0m     \u001b[0mj\u001b[0m \u001b[0;34m=\u001b[0m \u001b[0;36m1\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n",
-      "\u001b[0;31mIndexError\u001b[0m: list index out of range"
-     ]
-    }
-   ],
-   "source": [
-    "countingSort([2,45,1,5,2,4,7,8])"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {
-    "collapsed": true
-   },
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "anaconda-cloud": {},
-  "kernelspec": {
-   "display_name": "Python [Root]",
-   "language": "python",
-   "name": "Python [Root]"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.5.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 0
-}
+
+# coding: utf-8
+
+# In[38]:
+
+def countingSort(A):
+    
+    minimum = A[0]
+    maximum = A[0]
+    
+    #Find mimimum and maximum for range of index
+    for i in A:
+        if(i < minimum):
+            minimum = i
+        if(i > maximum):
+            maximum = i
+    
+    print ('Minimum Element: ', minimum)
+    print ('Maximum Element: ', maximum)  
+    
+    index = []
+    count = [0] * (maximum + 1)
+    
+    #First make all the elements 0 in count array
+    for i in range(minimum, maximum):
+        count[i] = 0 
+        
+    #Count occurences of each no in array 
+    for i in A:
+        count[A[i]] += 1
+        
+    
+    
+    
+    
+    count_array = [minimum, maximum]
+    j = minimum
+    
+    for i in range(minimum, maximum):
+        if(i == ):
+            count_array[i] = count[A[j]] 
+    
+        
+    
+
+        
+    
+
+
+# In[39]:
+
+countingSort([2,45,1,5,2,4,7,8])
+
+
+# In[ ]:
+
+
+
